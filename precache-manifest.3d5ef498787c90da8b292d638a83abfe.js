ef9976a17c0e6f1a4926515d0f96a318e24bc211
samyak3009 <samyak3009@gmail.com>
2022-07-31 15:15:12 +0530

    Updates

---
+++ precache-manifest.3d5ef498787c90da8b292d638a83abfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6fcdd055a3d1d19734f936eb3f6ef0",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "31392b7bee8245437819",
+    "url": "/mysite/static/css/main.1109858f.chunk.css"
+  },
+  {
+    "revision": "d9350babb08e22966fe4",
+    "url": "/mysite/static/js/2.229eb391.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mysite/static/js/2.229eb391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31392b7bee8245437819",
+    "url": "/mysite/static/js/main.0d31d526.chunk.js"
+  },
+  {
+    "revision": "0ab8e3b6faad384db959",
+    "url": "/mysite/static/js/runtime-main.23329acc.js"
+  }
+]);
